f984f3e583a4ff50180f28a19c903670719e0872
RamanshuPSingh <140968542+prof-wanderful@users.noreply.github.com>
2024-07-23 13:39:45 +0530

    update blog

--- blog_script.js
+++ blog_script.js
@@ -6,8 +6,15 @@ var img = document.querySelector(".blog-photo");
 var modalImg = document.getElementById("modal-img");
 
 img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
+    var collapsibleText = document.querySelector(".collapsible");
+    if (collapsibleText.style.display === "none" || collapsibleText.style.display === "") {
+        collapsibleText.style.display = "block";
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    } else {
+        collapsibleText.style.display = "none";
+        modal.style.display = "none";
+    }
 }
 
 // Get the <span> element that closes the modal
@@ -15,5 +22,7 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+    var collapsibleText = document.querySelector(".collapsible");
+    collapsibleText.style.display = "none";
     modal.style.display = "none";
 }
